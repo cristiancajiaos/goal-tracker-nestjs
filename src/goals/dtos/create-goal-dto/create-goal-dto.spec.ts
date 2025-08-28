a74722f1c1ec726f220b2dcd5d901058605bdaeb
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-28 10:23:41 -0400

    Creada clase Payload create-goal-dto

---
+++ src/goals/dtos/create-goal-dto/create-goal-dto.spec.ts
@@ -0,0 +1,7 @@
+import { CreateGoalDto } from './create-goal-dto';
+
+describe('CreateGoalDto', () => {
+  it('should be defined', () => {
+    expect(new CreateGoalDto()).toBeDefined();
+  });
+});
