3f312d525e7775be87d162b54101f1efefed81aa
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-28 10:10:47 -0400

    Añadidas respuestas a métodos del controlador

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,13 +1,22 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('goals')
 export class GoalsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      {id: 1, name: 'Goal 1'},
+      {id: 2, name: 'Goal 2'},
+      {id: 3, name: 'Goal 3'}
+    ];
+  }
 
   @Get(":id")
   findOne(@Param("id") id) {
-    return id;
+    return {
+      id: 1, 
+      name: 'Goal 1'
+    };
   }
 
   @Post()
@@ -16,9 +25,12 @@ export class GoalsController {
   }
 
   @Patch(":id")
-  update(@Param("id") id, @Body() input) {}
+  update(@Param("id") id, @Body() input) {
+    return input
+  }
   
   @Delete(":id")
+  @HttpCode(204)
   remove(@Param("id") id) {}
 
 }
