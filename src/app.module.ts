8534b473f3c76c5d3567140e55c68ccf3a02d88d
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-28 16:08:51 -0400

    Agregada configuración de DB en el módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoalsController } from './goals/goals.controller';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfigProd from './config/orm.config.prod';
+import { Goal } from './goals/entities/goal/goal';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd
+    }),
+    TypeOrmModule.forFeature([Goal])
+  ],
   controllers: [AppController, GoalsController],
   providers: [AppService],
 })
